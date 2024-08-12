dfe22a042dfc126ef2886d85054cd88d287c16da
XenoZ <xenoz0096@gmail.com>
2024-08-12 17:52:05 +0530

    Name

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link to the favicon */}
+        <link rel="icon" href="/placeholder.svg" />
+        {/* Default title if not set on specific pages */}
+        <title>GenAI Learning</title>
+        {/* You can add other meta tags or link tags here if needed */}
+      </Head>
       <body>
         <Main />
         <NextScript />
