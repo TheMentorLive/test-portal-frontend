fe430617d7d7ac64f4256c3100edae0891a7f1d8
XenoZ <xenoz0096@gmail.com>
2024-08-13 16:40:29 +0530

    Final Push

--- pages/Main/hero.js
+++ pages/Main/hero.js
@@ -19,7 +19,7 @@ export default function Hero() {
             </p>
             <br/>
             <br/>
-            <div className="flex flex-col gap-9 min-[400px]:flex-row ml-36 ">
+            <div className="flex flex-col gap-9 min-[400px]:flex-row ml-24 ">
               <Link
                 href=""
                 className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
