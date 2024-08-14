fe3015e89c2c7a5bf70a5336258f519943ab559a
Vishu0106 <kattameedivishnu2003@gmail.com>
2024-08-14 18:04:28 +0530

    Base URL changed

--- config/axiosInstance.js
+++ config/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8000/api/v1'
+// const BASE_URL = 'http://localhost:8000/api/v1'
+const BASE_URL = 'https://test-platform-backend.onrender.com/api/v1'
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
