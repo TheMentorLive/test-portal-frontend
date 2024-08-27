dae94a1ce84f7074a6cd40d83644383ea9b74525
XenoZ <xenoz0096@gmail.com>
2024-08-27 16:56:56 +0530

    Main pg update

---
+++ pages/Main/t2.js
@@ -0,0 +1,72 @@
+// "use client";
+// import React from "react";
+// import { HoverBorderGradient } from "../../components/ui/hover-border-gradient";
+
+// export default function HoverBorderGradientDemo() {
+//   return (
+//     (<div className="m-40 h-40 flex bg-black justify-center text-center">
+//       <HoverBorderGradient
+//         containerClassName="rounded-full"
+//         as="button"
+//         className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2">
+//         <AceternityLogo />
+//         <span>Aceternity UI</span>
+//       </HoverBorderGradient>
+//       <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+//           Shimmer
+//         </button>
+
+//         <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+//   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+//   <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+//     Border Magic
+//   </span>
+// </button>
+
+
+
+
+
+// {/* <Button 
+//                     type="submit" 
+//                     className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50" 
+//                     disabled={isSubmitting}
+//                   >
+//                     {isSubmitting ? (
+//                       <>
+//                         <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+//                         Submitting...
+//                       </>
+//                     ) : (
+//                       <>
+//                         <Send className="mr-2 h-4 w-4" />
+                        
+//                         Start Learning
+//                       </>
+//                     )}
+//                   </Button> */}
+//     </div>)
+//   );
+// }
+
+
+
+
+// const AceternityLogo = () => {
+//   return (
+//     (<svg
+//       width="66"
+//       height="65"
+//       viewBox="0 0 66 65"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg"
+//       className="h-3 w-3 text-black dark:text-white">
+//       <path
+//         d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
+//         stroke="currentColor"
+//         strokeWidth="15"
+//         strokeMiterlimit="3.86874"
+//         strokeLinecap="round" />
+//     </svg>)
+//   );
+// };
