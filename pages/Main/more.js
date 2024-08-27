e596d7acb40a5e2e4dc3428065178a63f65fd174
XenoZ <xenoz0096@gmail.com>
2024-08-27 17:50:21 +0530

    More Sec - update

--- pages/Main/more.js
+++ pages/Main/more.js
@@ -1,46 +1,49 @@
 "use client";
 import Link from "next/link";
 import { TypewriterEffectSmooth } from "../../public/ui/typewriter-effect";
+
 export default function More() {
   const words = [
     {
-      text: "Unlock ‎ ",
+      text: "Unlock ",
+      className: "text-[47px]", // Adjust text size here
     },
     {
-      text: "your ‎ ",
+      text: "your ",
+      className: "text-[47px]", // Adjust text size here
     },
     {
-      text: "potential ‎  ",
+      text: "potential ",
+      className: "text-[47px]", // Adjust text size here
     },
     {
-      text: "with ‎  ",
+      text: "with ",
+      className: "text-[47px]", // Adjust text size here
     },
     {
       text: "GenAi Learning.",
-      className: "text-blue-500 dark:text-blue-500",
+      className: "text-[47px] text-blue-500 dark:text-blue-500", // Adjust text size here
     },
   ];
+
   return (
-    (<div className="flex flex-col items-center justify-center h-[40rem]  ">
-      <p className="text-neutral-900 dark:text-neutral-900 text-xs sm:text-base  ">
-      Your journey to a brighter future begins here
+    <div className="flex flex-col items-center justify-center h-[40rem]">
+      <p className="text-neutral-900 dark:text-neutral-900 text-xs sm:text-base mb-4">
+        Your journey to a brighter future begins here
       </p>
-      <TypewriterEffectSmooth words={words} />
-      <br/>
-      <br/>
-      <div
-        className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <button
-          className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+      <TypewriterEffectSmooth words={words} className="text-2xl" /> {/* Adjust text size here */}
+      <br />
+      <br />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
           Contact Us
         </button>
         <Link href="/Main/signup">
-        <button
-          className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
-          Signup
-        </button>
+          <button className="w-40 h-10 rounded-xl bg-white text-black border border-black text-sm">
+            Signup
+          </button>
         </Link>
       </div>
-    </div>)
+    </div>
   );
 }
