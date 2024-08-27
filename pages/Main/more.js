3f5e7516313004aaa27904112f8a457357b17fb6
XenoZ <xenoz0096@gmail.com>
2024-08-27 18:17:00 +0530

    More Sec Update

--- pages/Main/more.js
+++ pages/Main/more.js
@@ -31,8 +31,8 @@ export default function More() {
       <p className="text-neutral-900 dark:text-neutral-900 text-xs sm:text-base">
       Your journey to a brighter future begins here
       </p>
-      <br/>
-      <TypewriterEffectSmooth words={words} />
+      
+      <TypewriterEffectSmooth words={words} className="mt-5" />
       <br/>
       <br/>
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
